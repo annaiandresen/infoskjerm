4a63cfa5883d9580ad3e62f8d9307dd9f35b75fb
Anna Irene Andresen <annaian@stud.ntnu.no>
2020-09-10 18:00:48 +0200

    Updates

---
+++ precache-manifest.4fe6570290a8ea81fac8e2d29a5b807e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32db915de77384be9de53c9511bf03c4",
+    "url": "/infoskjerm/index.html"
+  },
+  {
+    "revision": "265245e51efd7d322353",
+    "url": "/infoskjerm/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0979a2810cf121b4a0ab",
+    "url": "/infoskjerm/static/js/2.2a516453.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/infoskjerm/static/js/2.2a516453.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "265245e51efd7d322353",
+    "url": "/infoskjerm/static/js/main.0a031159.chunk.js"
+  },
+  {
+    "revision": "6fa4fe89daa3d5d07bae",
+    "url": "/infoskjerm/static/js/runtime-main.d6c9a5ab.js"
+  }
+]);
