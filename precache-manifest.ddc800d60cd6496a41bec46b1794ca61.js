a8f71353c5b23cb6465c1f5a5ae46ec6a5b4ffb8
Anna Andresen <anna.irene.andresen@gmail.com>
2021-10-01 21:28:15 +0200

    Updates

---
+++ precache-manifest.ddc800d60cd6496a41bec46b1794ca61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c7d6ea6bae68fd5cf872350c95a9238",
+    "url": "/infoskjerm/index.html"
+  },
+  {
+    "revision": "8e435475c6884fcaefd5",
+    "url": "/infoskjerm/static/css/main.e5464ebd.chunk.css"
+  },
+  {
+    "revision": "8659a90a3d05e3fa3a48",
+    "url": "/infoskjerm/static/js/2.fc507248.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/infoskjerm/static/js/2.fc507248.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e435475c6884fcaefd5",
+    "url": "/infoskjerm/static/js/main.2fd604e9.chunk.js"
+  },
+  {
+    "revision": "6fa4fe89daa3d5d07bae",
+    "url": "/infoskjerm/static/js/runtime-main.d6c9a5ab.js"
+  }
+]);
